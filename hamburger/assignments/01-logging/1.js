e9b74b7a2ed0d0da88383d20a1a67b7100fc3ad9
vivit git <vivitputu2001@gmail.com>
2026-08-26 19:38:47 +0800

    Add logging assignments

---
+++ hamburger/assignments/01-logging/1.js
@@ -0,0 +1,3 @@
+console.log("Welcome to JavaScript");
+console.log("I am a programmer");
+console.log("Programming is fun");
---
+++ hamburger/assignments/01-logging/2.js
@@ -0,0 +1,4 @@
+console.log("    J     A     V     V     A");
+console.log("    J    A A     V   V     A A");
+console.log("J   J   AAAAA     V V     AAAAA");
+console.log(" J J   A     A     V     A     A");
